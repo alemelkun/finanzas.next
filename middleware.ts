ee95a5a5af07a82465ba1ad7ea7fc8f703710944
nacho <nachomelkun@gmail.com>
2026-04-05 21:15:36 -0300

    fix: deshabilitar middleware

--- middleware.ts
+++ middleware.ts
@@ -2,16 +2,9 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-  const isLoginPage = req.nextUrl.pathname === '/login'
-  const hasCookie = req.cookies.getAll().some(c => c.name.includes('auth'))
-
-  if (!hasCookie && !isLoginPage) {
-    return NextResponse.redirect(new URL('/login', req.url))
-  }
-
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|api).*)'],
+  matcher: [],
 }
